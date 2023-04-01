aaa9ec907a42c22ddad4ff3d5e0111a50a1c6ee1
ashutosh8172 <ashutoshojha8172@gmail.com>
2023-04-01 17:25:58 +0530

    Add existing project files to Git

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { Io5Icon } from "react-icons/io5";
+import axios from "axios";
+
+import styled from "styled-components";
+import "./App.css";
+import Header from "./components/header";
+import { SearchBar } from "./components/searchBar";
+
+const AppContainer = styled.div`
+  margin: auto;
+  padding: 0 530px;
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <AppContainer>
+        <Header></Header>
+        <SearchBar></SearchBar>
+      </AppContainer>
+    </>
   );
 }
 
